b71c609d4c36f1e6dc43ef8f0ce6144d902e7ebd
leeyihao96 <yihao96@hotmail.com>
2016-12-25 13:54:26 +0800

    Add files via upload

---
+++ UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using lkhassignment;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Arrange
+            menu menu = new menu();
+            //Act
+            ViewResult result = menu as ViewResult;
+            //Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
